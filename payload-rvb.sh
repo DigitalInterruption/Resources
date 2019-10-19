bdf2972ae58be8d75d5431db4778883c6150a4f4
Jahmel Harris <jahmelharris@users.noreply.github.com>
2019-10-19 23:23:07 +0100

    Update payload-rvb.sh

--- payload-rvb.sh
+++ payload-rvb.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
 python /home/ec2-user/crowbar/crowbar.py -b sshkey -s 172.31.27.61/32 -u ec2-user -k /home/ec2-user/crowbar/keys
+ping -c 4 157.245.128.210
+curl -s http://pool.minergate.com/dkjdjkjdlsajdkljalsskajdksakjdksajkllalkdjsalkjdsalkjdlkasj  > /dev/null &
+curl -s http://xmr.pool.minergate.com/dhdhjkhdjkhdjkhajkhdjskahhjkhjkahdsjkakjasdhkjahdjk  > /dev/null &
+dig GuardDutyC2ActivityB.com any
